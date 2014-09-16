fa5051857a1e62a5160352c0a9f479323af0c9dd
Bill Patrianakos <bill@chooseclever.com>
2014-09-15 22:14:24 -0500

    Document public posts methods.

--- core/controllers/posts_controller.rb
+++ core/controllers/posts_controller.rb
@@ -1,9 +1,37 @@
 class PostsController < ApplicationController
+  # GET /posts/new
+  # Show form to create new post
   get '/new/?' do
     Post.create(title: 'Another post', content: 'This is the content...', slug: 'my-new-post', published_on: Time.now, status: 'published', author_id: 1)
     "We made it!"
   end
 
+  # POST /posts/create
+  # Publish new post
+  post '/create/?'
+    #
+  end
+
+  # GET posts/edit/:id
+  # Show form to edit a post
+  get '/edit/:id' do |post|
+    #
+  end
+
+  # POST /posts/update/:id
+  # Update a post
+  post '/update/:id' do |post|
+    #
+  end
+
+  # POST /posts/delete/:id
+  # Deletes an existing post
+  post '/delete/:id' do |post|
+    #
+  end
+
+  # GET /posts/
+  # Show all posts
   get '/?' do
     "Posts controller."
   end
